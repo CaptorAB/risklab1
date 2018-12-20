0f1e8a248be439765f60f998ddec29f9e0f3c306
Tor Nordqvist <tor.nordqvist@captor.se>
2018-12-20 21:58:18 +0100

    first commit (moved from private repo nodejs)

---
+++ README.md
@@ -0,0 +1,18 @@
+# Risklab 1
+
+Detta risklab demonstrerar risk mot möjlig avkastning för olika risknivåer och olika tidshorizonter. Välj den risknivå som du känner dig bekväm med.
+
+This risklab shows risk vs return for different risk levels and time frames.
+
+Time series is updated until 2014.
+
+## How to run
+
+Do git clone.
+
+Then
+
+```
+npm i
+npm start
+```
---
+++ server.js
@@ -0,0 +1,23 @@
+﻿var express = require("express");
+var path = require("path");
+var qs = require("qs");
+var getPort = require('get-port');
+
+var app = express();
+app.use(express.static(path.join(__dirname, "dist")));
+
+var server;
+
+async function start() {
+    var port = await getPort({port: [...Array(12)].map((d,i)=>3000+i)});
+    server = app.listen(port, null, () => {
+        var { address, port } = server.address();
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}
+start();
+
+app.get("/risklab/feedback", (req, res) => {
+    // do nothing
+    console.log(JSON.stringify(req.url));
+});
